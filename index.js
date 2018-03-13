7c94aca5804e7dbcfed38b77451dcd8a5e313607
Michael Roth <mick2090@gmail.com>
2018-03-13 23:49:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  const lis = document.getElementById('app').querySelector('div#grand-node')
+  const lis = document.getElementById('app').querySelector('div#grand-node').querySelectorAll('div');
  
   for (let i = 0; i < lis.length; i++) {
     if(i === lis.length){
