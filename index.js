23df48490e6dc0fddd5cbaa42f5dbdfc896c2dc4
Michael Roth <mick2090@gmail.com>
2018-03-13 22:38:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  document.getElementById('nested').getElementByClassName('div.target');
+  return document.getElementById('nested').getElementsByClassName('div.target');
   
   
 }
