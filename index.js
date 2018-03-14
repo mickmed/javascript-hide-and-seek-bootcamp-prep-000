49d37ab6505ba3dd9ea899a38bb1e0629bd0017d
Michael Roth <mick2090@gmail.com>
2018-03-14 00:20:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  const lis = document.getElementById('app').querySelector('div#grand-node').querySelectorAll('div');
+ 
+  for (let i = 0; i < lis.length; i++) {
+    if(i+1 === lis.length){
+     
+      return (lis[i]);
+    }
+  }
+  
+}
